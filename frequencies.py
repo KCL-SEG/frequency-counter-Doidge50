627641a0c39823740fa6567cfdcbb05801d63ef3
Adam Doidge <adam.doidge@kcl.ac.uk>
2022-10-05 16:08:10 +0100

    Complete assignment

--- frequencies.py
+++ frequencies.py
@@ -1,7 +1,16 @@
 """Frequencies function."""
 """ENTER YOUR SOLUTION HERE!"""
 
-def frequencies(items):
+def frequencies(*args):
+
+    args_list =[]
+    for item in args:
+        args_list.append(item)
+
     frequencies = {}
-    # Your code goes here
+    for item in args_list:
+        frequencies.update({item : args_list.count(item)})
+    
     return frequencies
+
+print(frequencies(1,2,4,5,"hello",1,1,"hello"))
